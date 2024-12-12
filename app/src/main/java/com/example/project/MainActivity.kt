d048ecb6337df9afcd8af0329233f96e3dc2ab6d
Hayeon-Lee <papepopepe@naver.com>
2024-12-12 16:34:16 +0900

    [feature]Add a event when clicked the button

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.example.project
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.project.ui.theme.ProjectTheme
-
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
+
+        button = findViewById(R.id.myButton)
+        button.setOnClickListener{
+            Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
