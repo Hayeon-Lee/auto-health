2f9e57a423fc139dc7e4c94aff2f6aa00237418a
hayeon-lee <papepopepe@naver.com>
2024-12-16 08:35:38 +0900

    [feature] 인식한 글자를 2번째 화면으로 전달.

--- app/src/main/java/com/example/project/SecondActivity.kt
+++ app/src/main/java/com/example/project/SecondActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
 
@@ -11,10 +12,11 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // Intent로부터 전달된 데이터 수신
+        val response = intent.getStringExtra("response")
+
+        // 값 확인 및 화면에 표시
+        val textView = findViewById<TextView>(R.id.responseTextView)
+        textView.text = response ?: "데이터 없음"
     }
 }
